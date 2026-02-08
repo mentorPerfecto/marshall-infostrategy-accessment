f1297bc056eef80e26bf42311ae6a215fd271f09
MENTOR <odiichibueze7@gmail.com>
2026-02-08 05:24:42 +0100

    Update Android project configuration and rename package

---
+++ android/app/src/main/kotlin/com/example/marshall_infostrategy_accessment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.marshall_infostrategy_accessment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/mobile_assessment/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mobile_assessment
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
